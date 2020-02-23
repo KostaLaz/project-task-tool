5116c2429ce700dc08cd143b6e82722fd06b2f57
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2020-02-23 19:54:38 +0100

    Initial commit

---
+++ src/main/java/com/demo/projecttasktool/ProjectTaskToolApplication.java
@@ -0,0 +1,13 @@
+package com.demo.projecttasktool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTaskToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTaskToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/projecttasktool/ProjectTaskToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.projecttasktool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaskToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
