e4f8d0a90006336e6b78d5ce7c90b30e5b656706
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2020-02-24 19:43:24 +0100

    Commit

--- src/main/java/com/demo/projecttasktool/domain/ProjectTask.java
+++ src/main/java/com/demo/projecttasktool/domain/ProjectTask.java
@@ -1,17 +1,26 @@
 package com.demo.projecttasktool.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ProjectTask {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Summary can not be blank.")
     private String summary;
 
     private String acceptanceCriteria;
--- src/main/java/com/demo/projecttasktool/service/ProjectTaskService.java
+++ src/main/java/com/demo/projecttasktool/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.demo.projecttasktool.service;
 
+import com.demo.projecttasktool.domain.ProjectTask;
 import com.demo.projecttasktool.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,14 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask){
+
+        if(projectTask.getStatus() == null || projectTask.getStatus() == ""){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
 
 }
