e1bd02d776d4cd666a3934e4e32436557005a59f
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2020-02-26 17:45:50 +0100

    Routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import NavBar from './components/NavBar';
+import ProjectBoard from './components/ProjectBoard';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import AddProjectTask from './components/projectTask/AddProjectTask';
 
-function App() {
+
+class App extends Component {
+  render(){
   return (
+    <BrowserRouter>
     <div className="App">
 
-        <h1 className="alert alert-warning">Welcome to the Project Task Project</h1>
-
+     <NavBar/>
+     <Route exact path="/" component={ProjectBoard}/>
+     <Route exact path="/addProjectTask" component={AddProjectTask}/>
     </div> 
-  );
+    </BrowserRouter>
+    
+    );
+}
 }
 
 export default App;
