5842a359380b610c0492c3a9bfa25214c36c2c02
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2020-03-04 17:10:41 +0100

    Update action

--- app/src/components/projectTask/UpdateProjectTask.js
+++ app/src/components/projectTask/UpdateProjectTask.js
@@ -7,6 +7,17 @@ import {getProjectTask} from "../../actions/projectTaskActions";
 
  class UpdateProjectTask extends Component {
 
+    constructor(){
+        super();
+
+        this.state = {
+            summary: "",
+            acceptanceCriteria: "",
+            status: "",
+            errors: {}
+        }
+    }
+
     componentDidMount(){
         const {id} = this.props.match.params;
         console.log(this.props.match.params+ "!!!!!!!!!!!!!!!");
@@ -26,13 +37,14 @@ import {getProjectTask} from "../../actions/projectTaskActions";
                     <h4 className="display-4 text-center">Add /Update Project Task</h4>
                     <form>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" name="summary" placeholder="Project Task summary" />
+                            <input type="text" className="form-control form-control-lg" name="summary" 
+                            value={this.state.summary}placeholder="Project Task summary" />
                         </div>
                         <div className="form-group">
-                            <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria"></textarea>
+                            <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" name="acceptanceCriteria" value={this.state.acceptanceCriteria}></textarea>
                         </div>
                         <div className="form-group">
-                            <select className="form-control form-control-lg" name="status">
+                            <select className="form-control form-control-lg" name="status" value={this.state.status}>
                                 <option value="">Select Status</option>
                                 <option value="TO_DO">TO DO</option>
                                 <option value="IN_PROGRESS">IN PROGRESS</option>
